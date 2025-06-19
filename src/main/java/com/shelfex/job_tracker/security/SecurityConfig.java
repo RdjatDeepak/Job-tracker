244a34e81d58b3d4adc7dc1b8ca4e0147097d9a0
RdjatDeepak <125497098+RdjatDeepak@users.noreply.github.com>
2025-06-19 15:34:42 +0530

    Gmail Updation
    real time notification through email send notifications

--- src/main/java/com/shelfex/job_tracker/security/SecurityConfig.java
+++ src/main/java/com/shelfex/job_tracker/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**" ,"/api/email/test").permitAll()
                         .anyRequest().authenticated())
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint(
